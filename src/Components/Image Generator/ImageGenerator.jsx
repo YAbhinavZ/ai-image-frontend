c35a5130239b663c8f7939e5717df2de03639a60
YAbhinavZ <abhinavvirtuoso@gmail.com>
2026-02-07 22:31:40 +0530

    some fixes

--- src/Components/Image Generator/ImageGenerator.jsx
+++ src/Components/Image Generator/ImageGenerator.jsx
@@ -15,7 +15,7 @@ export const ImageGenerator = () => {
     setError("");
 
     try {
-      const res = await fetch("https://ai-image-backend-qcoy.onrender.com", {
+      const res = await fetch("https://ai-image-backend-qcoy.onrender.com/api/generate", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ prompt }),
